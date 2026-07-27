abfb8e1417c06ed6849b05978ab33932f2f61645
Raghav Khandelwal <raaghav.khandelwal@gmail.com>
2026-07-26 17:32:18 -0700

    server

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,25 @@
 package main
 
+import (
+	"log/slog"
+	"os"
+)
+
 func main() {
 	cfg := config{
 		addr: ":8080",
 		db:   dbConfig{},
 	}
 
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+
 	api := application{
 		config: cfg,
+	}
+
+	if err := api.run(api.mount()); err != nil {
+		slog.Error("server failed to start", "error", err)
+		os.Exit(1)
 	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Raghav847/ecom-api
 
 go 1.25.0
+
+require github.com/go-chi/chi/v5 v5.3.1 // indirect
